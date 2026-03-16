c1de3a3f9179fa97d737e327e26b8e2dabbdded3
teju81 <teju81@gmail.com>
2026-03-16 17:33:56 +0530

    Add files via upload

--- scene/dataset_readers.py
+++ scene/dataset_readers.py
@@ -95,6 +95,8 @@ def readColmapCameras(cam_extrinsics, cam_intrinsics, images_folder):
             assert False, "Colmap camera model not handled: only undistorted datasets (PINHOLE or SIMPLE_PINHOLE cameras) supported!"
 
         image_path = os.path.join(images_folder, os.path.basename(extr.name))
+        if not os.path.exists(image_path):
+            image_path = os.path.join(images_folder, extr.name)
         image_name = os.path.basename(image_path).split(".")[0]
         image = Image.open(image_path)
 
